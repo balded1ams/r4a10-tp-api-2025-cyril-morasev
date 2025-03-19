cc75dc505221c84e8473522a0785bf7363aa6d6f
morasev <evan.moras@etu.univ-grenoble-alpes.fr>
2025-03-19 23:18:16 +0100

    changement_css pour sections et ajout des methodes dans le main.js pour le prechargement de chaque élément de catégorie et son clic

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const categorySection = document.querySelector(".choice_category");
     const listSection = document.querySelector("main ul");
+    const mainContent = document.querySelector("main");
+    const linkelements = document.querySelectorAll("link[rel=preload]");
 
     const promises = categories.map(async (categoryName) => {
         const categoryInstance = new Category(categoryName);
@@ -21,6 +23,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             <img src="${imageUrl}" alt="${categoryName}">
             <h2>${categoryName}</h2>
         `;
+        categoryDiv.id = categoryName;
         
         const preloadImages = () => {
             for (let i = 1; i < 21; i++) {
@@ -37,10 +40,45 @@ document.addEventListener("DOMContentLoaded", async () => {
         categorySection.appendChild(categoryDiv);
 
         const listItem = document.createElement("li");
-        listItem.textContent = categoryName;
+        const a = document.createElement("a");
+        a.href = `#${categoryName}`;
+        a.textContent = categoryName;
+        listItem.appendChild(a);
         listSection.appendChild(listItem);
+
+
+        
+        linkelements.forEach((link) => {
+            const preloadElements = () => {
+                if (!link.dataset.element) {
+                    const divElement = document.createElement("div");
+                    divElement.classList.add("element");
+                    const item = categoryInstance.content[0]; 
+
+                    divElement.innerHTML = `
+                        <img src="${item.image}" alt="${categoryName}">
+                        <p>${item.description}</p>
+                        <p>${item.location}</p>
+                    `;
+
+                    link.dataset.element = divElement.outerHTML;
+                }
+                link.removeEventListener("mouseenter", preloadElements);
+            };
+
+            const showElements = () => {
+                if (link.dataset.element) {
+                    mainContent.innerHTML = link.dataset.element;
+                }
+                link.removeEventListener("click", showElements);
+            };
+
+            link.addEventListener("mouseenter", preloadElements);
+            link.addEventListener("click", showElements);
+        });
     });
 
     await Promise.all(promises);
+
 });
 
